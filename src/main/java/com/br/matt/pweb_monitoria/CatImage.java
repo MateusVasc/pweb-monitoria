f80512c0259a81c4564058890f2072bc7621423b
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-08-22 15:19:33 -0300

    feat: Adds catImage class

---
+++ src/main/java/com/br/matt/pweb_monitoria/CatImage.java
@@ -0,0 +1,15 @@
+package com.br.matt.pweb_monitoria;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CatImage {
+
+  private String id;
+  private String url;
+  private int width;
+  private int height;
+
+}
