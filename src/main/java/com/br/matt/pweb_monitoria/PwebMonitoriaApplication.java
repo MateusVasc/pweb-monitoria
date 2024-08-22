c222a066daba9266a6625349bb14a8dfbf0839e8
MateusVasc <109766889+MateusVasc@users.noreply.github.com>
2024-08-22 10:33:21 -0300

    project initial structure

---
+++ src/main/java/com/br/matt/pweb_monitoria/PwebMonitoriaApplication.java
@@ -0,0 +1,13 @@
+package com.br.matt.pweb_monitoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwebMonitoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwebMonitoriaApplication.class, args);
+	}
+
+}
